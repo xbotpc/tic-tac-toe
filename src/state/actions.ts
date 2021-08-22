e889d274a2c5429733740d530f626081de621a54
Piyush <piyush2396@gmail.com>
2021-08-22 18:39:44 +0530

    Updates

--- src/state/actions.ts
+++
@@ -1,8 +0,0 @@
-const actions = {
-    PLAY_PAUSE: 'PLAY_PAUSE',
-    CHANGE_PLAYBACK_AUDIO: 'CHANGE_AUDIO_FILE',
-    ADD_SONG: 'ADD_SONG',
-    REMOVE_SONG: 'REMOVE_SONG'
-}
-
-export { actions as ACTIONS };
